c48f2479066d1763deac32b381e8922ef4f0641d
Nagy Kristóf <nagynandorkristof@gmail.com>
2025-06-26 01:11:58 +0200

    init

---
+++ src/components/HelloWorld.ce.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Button from "primevue/button"
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+  <Button :label="`You clicked me ${count} times`" @click="count++" class="mt-4" ></Button>
+</template>
